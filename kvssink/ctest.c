b6cc89d11400a29c6b7043baed7aed19a2dc389c
SBJakobsen <sorenbj1234@hotmail.com>
2023-03-20 23:08:18 +0100

    error messages

--- kvssink/ctest.c
+++ kvssink/ctest.c
@@ -32,8 +32,8 @@ int tutorial_main (int argc, char *argv[])
     
     /* Build the pipeline */
     gst_bin_add_many (GST_BIN (pipeline), source, mpph264enc, h264parse, kvssink, NULL);
-    
-    
+    g_printerr("Pipeline built");
+
     GstCaps *caps1;  
     caps1 = gst_caps_from_string("video/x-raw,width=640,height=480, framerate=30/1,format=YUY2");
     if (gst_element_link_filtered(source, mpph264enc, caps1) != TRUE) {
@@ -56,14 +56,15 @@ int tutorial_main (int argc, char *argv[])
         return -1;
     }
 
-    
-    // HEEEEEEEEEEEERE
-
+    g_print("Elements linked");
 
 
     /* Modify the source's properties */
     g_object_set (source, "device", "/dev/video4",
         "do-timestamp", true, NULL);
+    g_printerr ("mpph264enc and h264parse could not be linked\n");
+
+    g_print("Modified source's parameters")
 
     /* Modify the sink's properties */
     g_object_set (kvssink, 
@@ -74,6 +75,8 @@ int tutorial_main (int argc, char *argv[])
         "log-config", "/usr/src/app/kvs_log_configuration",
         "iot-certificate", "iot-certificate,endpoint=crhxlosa5p0oo.credentials.iot.eu-west-1.amazonaws.com,cert-path=usr/src/app/certs/cert.pem,key-path=usr/src/app/certs/privkey.pem,ca-path=usr/src/app/certs/root-CA.pem,role-aliases=fbview-kinesis-video-access-role-alias",
         NULL); 
+    
+    g_print("Modified sinks parameters");
  
 
     /* Start playing */
@@ -84,6 +87,8 @@ int tutorial_main (int argc, char *argv[])
         return -1;
     }
 
+    g_print("Started Playing");
+
     /* Wait until error or EOS */
     bus = gst_element_get_bus (pipeline);
     
@@ -91,28 +96,28 @@ int tutorial_main (int argc, char *argv[])
       GST_MESSAGE_ERROR | GST_MESSAGE_EOS);
 
     if (msg != NULL) {
-    GError *err;
-    gchar *debug_info;
+        GError *err;
+        gchar *debug_info;
 
-    switch (GST_MESSAGE_TYPE (msg)) {
-      case GST_MESSAGE_ERROR:
-        gst_message_parse_error (msg, &err, &debug_info);
-        g_printerr ("Error received from element %s: %s\n",
-            GST_OBJECT_NAME (msg->src), err->message);
-        g_printerr ("Debugging information: %s\n",
-            debug_info ? debug_info : "none");
-        g_clear_error (&err);
-        g_free (debug_info);
-        break;
-      case GST_MESSAGE_EOS:
-        g_print ("End-Of-Stream reached.\n");
-        break;
-      default:
-        /* We should not reach here because we only asked for ERRORs and EOS */
-        g_printerr ("Unexpected message received.\n");
-        break;
-    }
-    gst_message_unref (msg);
+        switch (GST_MESSAGE_TYPE (msg)) {
+        case GST_MESSAGE_ERROR:
+            gst_message_parse_error (msg, &err, &debug_info);
+            g_printerr ("Error received from element %s: %s\n",
+                GST_OBJECT_NAME (msg->src), err->message);
+            g_printerr ("Debugging information: %s\n",
+                debug_info ? debug_info : "none");
+            g_clear_error (&err);
+            g_free (debug_info);
+            break;
+        case GST_MESSAGE_EOS:
+            g_print ("End-Of-Stream reached.\n");
+            break;
+        default:
+            /* We should not reach here because we only asked for ERRORs and EOS */
+            g_printerr ("Unexpected message received.\n");
+            break;
+        }
+        gst_message_unref (msg);
   }
     
     stop_pipeline:

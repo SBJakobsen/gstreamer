adfb4d68c7b0d61a251b8f2b30fd5946746536aa
Soren <sobj@blackbird.online>
2023-03-29 15:30:04 +0200

    clean-up before give-up

--- mpp_kvssink_no_queue/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-rm -r /data/*
-./a.out | tee /data/fps.log
-#
-#mpp related and avdec
--- x264_kvssink/setenvs.sh
+++ x264_kvssink/setenvs.sh
@@ -3,11 +3,11 @@ export WIDTH=640
 export HEIGHT=480
 export FRAMERATE=30
 export RESIN_DEVICE_UUID=15e0dc81d12c414aa02b49b990921c8d
-export CERTSDIR=/home/soren/projects/gstreamer/desktop/certs
+export CERTSDIR=/home/soren/projects/gstreamer/x264_kvssink/certs
 export AWS_ENDPOINT=crhxlosa5p0oo.credentials.iot.eu-west-1.amazonaws.com
 export ROLE_ALIAS=fbview-kinesis-video-access-role-alias
 export AWS_REGION=eu-west-1
-
+export AWS_DEFAULT_REGION=eu-west-1
 
 # 
 #    char *p_width               = "WIDTH";
